db31db722e0a2694fc1ef71e5b17257f452855ba
abrahandev45 <105373938+abrahandev45@users.noreply.github.com>
2022-11-24 19:25:03 -0500

    actualizado

--- javascript.js
+++ javascript.js
@@ -1,83 +1,61 @@
 
-const btncilindro = document.getElementById('btncilindro');
-const btnesfera = document.getElementById('btnesfera');
-const btncubo = document.getElementById('btncubo');
-const cilin =document.querySelector('#cilin');
-const esfer = document.querySelector('#esfer');
-const cubi = document.querySelector('#cubo');
 
 
-btncilindro.addEventListener('click',function(){
-    cilin.classList.toggle('cilinmostrar');
-})
+var item =  Array.prototype.slice.apply(document.querySelectorAll('.item'));
+var panels = Array.prototype.slice.apply(document.querySelectorAll('.column-50'));
 
-btnesfera.addEventListener('click',function(){
-    esfer.classList.toggle('esfermostrar');
-})
-btncubo.addEventListener('click', function(){
-    cubi.classList.toggle('cubomostrar');
-})
+var link = document.getElementById('link').addEventListener('click',function(e){
+    
+    if(e.target.classList.contains('item')) {
+       
 
+       let i = item.indexOf(e.target);
+        
+        item.map(ite => ite.classList.remove('active'));
+        item[i].classList.add('active');
 
+        panels.map(panel => panel.classList.remove('active'));
+        panels[i].classList.add('active');
+
+    }
+});
+
+//operaciones
 
 const cilindro = document.querySelector('.cilindro');
 const esfera = document.querySelector('.esfera');
-const cubo = document.querySelector('.cubo');
+const cubo = document.querySelector('.cubox');
 
 
 cilindro.addEventListener('click',function(e){
-    e.preventDefault();
-    var radio = parseFloat(document.getElementById('radio').value);
+
     var altura = parseFloat(document.getElementById('altura').value);
-    
+    var radio = parseFloat(document.getElementById('radio').value);
     var pi = 3.1416;
-    
-     /*resultado.innerHTML = pi * (radio * radio) * altura;*/
-     document.getElementById('resultado').innerHTML = pi * (radio * radio) * altura; 
-   
-})
-
-esfera.addEventListener('click',function(e){
-
- var ciradio = parseFloat(document.getElementById('ciradio').value);
- var pi = 3.1416;
- e.preventDefault();
-
- document.getElementById('esferesultado').innerHTML = 4/3 * pi *(ciradio * ciradio * ciradio);
-  
-})
-
-cubo.addEventListener('click',function(e){
-
     e.preventDefault();
-    var cubolado = parseInt(document.getElementById('cubolado').value)
-    document.getElementById('cuboresultado').innerHTML = 6 * (cubolado * cubolado);
 
-}) 
-
-
-
-  
-
-// TAREA DE CASOS
-
-/*var numero1 = 5;
-var numero2 = 8;
-
-if(numero1 >! numero2 && numero2 > 0){
-    alert('numero'+' '+ numero1 +' no es mayor que'
-     + ' ' + numero2 + ' y' + ' '+ numero2 + ' ' + ' es positivo')
-}
-
-if( (numero1 < 0 || numero1 != 0)){
-    var numerot = numero1+1;
-    alert('el numero'+' '+ numero1 + ' ' + 
-    'es negativo o distinto de 0  e incrementar en unidad al valor '
-    + numerot + ' no lo hace mayor ó igual que numero' +' '+ numero2)
+    document.getElementById('cilinresultado').innerHTML = pi * (radio * radio)* altura;
     
-}*/
+    })
 
-// TAREA DE MENUS
+
+    esfera.addEventListener('click',function(e){
+
+        var ciradio = parseFloat(document.getElementById('ciradio').value);
+        var pi = 3.1416;
+        e.preventDefault();
+       
+        document.getElementById('esferesultado').innerHTML = 4/3 * pi *(ciradio * ciradio * ciradio);
+        })
+
+    cubo.classList.toggle('cubomostrar');
+
+        cubo.addEventListener('click',function(e){
+            e.preventDefault();
+            var cubolado = parseInt(document.getElementById('cubolado').value)
+            document.getElementById('cuboresultado').innerHTML = 6 * (cubolado * cubolado);
+        })
+
 
 
 
